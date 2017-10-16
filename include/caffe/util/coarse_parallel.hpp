b0230122f302f85f3feb88b43682f9fdcdeb7669
Hao Fu <haofu@tju.edu.cn>
2017-10-16 16:39:08 +0800

    Remove the coarse_parallel.hpp file.

--- include/caffe/util/coarse_parallel.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef CPU_ONLY
-
-#ifndef COARSE_PARALLEL_HPP_
-#define COARSE_PARALLEL_HPP_
-
-#endif COARSE_PARALLEL_HPP_   /** COARSE_PARALLEL_HPP_ setting. **/
-
-#endif CPU_ONLY    /** CPU_ONLY setting. **/
